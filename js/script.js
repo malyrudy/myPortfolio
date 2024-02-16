d7d073112d01be17ed12092fa22ea213aa05b378
malyrudy <98644282+malyrudy@users.noreply.github.com>
2024-02-16 11:27:31 +0100

    pridani loga, thankYouPage, formSubmit, změněno h3 v #home, zkrácení textu v sluzby-box, uprava portfolio section(odstraneni layer

--- js/script.js
+++ js/script.js
@@ -50,7 +50,7 @@ ScrollReveal().reveal('.home-content p, .oMe-content, .kontakt h3', { origin: "r
 
 // Typed js 
 const typed = new Typed(".multiple-text", {
-    strings: ["Webový vývojář", "Edukační tvůrce"],
+    strings: ["Webový vývojář", "Webový designér", "Webový správce"],
     typeSpeed: 100, 
     backSpeed: 100, 
     backDelay: 1000,
